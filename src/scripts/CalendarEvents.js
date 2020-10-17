7021174fc75c4bc3a3dd679fa9149c7b972f26ce
Zondazx <mzxgd16@gmail.com>
2020-10-17 14:25:38 -0400

    refactor: incosistency in css

--- src/scripts/CalendarEvents.js
+++ src/scripts/CalendarEvents.js
@@ -2,7 +2,6 @@ import "../styles/modalbox.css";
 
 const calendarDays = document.getElementById("calendar-days");
 const modalBox = document.querySelector(".modal");
-const modalBoxContent = document.querySelector(".modal .content"); // test
 const closeModalBox = document.querySelector(".close");
 
 function handleDayClick() {
@@ -20,6 +19,13 @@ function handleDayClick() {
 function displayModalBox(e) {
     if (e.target.classList.contains("calendar-day--not-current")) return;
         
+    const modalBoxContent = modalBox.querySelector(".modal-content");
+    const title = modalBoxContent.querySelector(".input-title");
+    const description = modalBoxContent.querySelector(".input-description");
+    const duedate = modalBoxContent.querySelector(".input-duedate");
+    // console.log(modalBoxContent.querySelector(".input-title"));
+
+
     modalBox.style.display = "block";
     // modalBoxContent.textContent = `Day #${e.target.textContent}`;
     // const eventFlag = e.target.querySelector(".event-flag");
