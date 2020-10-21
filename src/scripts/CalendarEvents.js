63ef285527ae53d19ddccd8006969b1804e6d82d
Zondazx <mzxgd16@gmail.com>
2020-10-21 15:00:22 -0400

    feat: events can be updated

--- src/scripts/CalendarEvents.js
+++ src/scripts/CalendarEvents.js
@@ -1,48 +1,74 @@
 import "../styles/modalbox.css";
 
+import dayjs from "dayjs";
+
 const calendarDays = document.getElementById("calendar-days");
 const modalBox = document.querySelector(".modal");
 const closeModalBox = document.querySelector(".close");
 
-const modalBoxContent = modalBox.querySelector(".modal-content");
-const title = modalBoxContent.querySelector(".input-title");
-const titleLabel = modalBoxContent.querySelector(".label-title");
-const description = modalBoxContent.querySelector(".input-description");
-const duedate = modalBoxContent.querySelector(".input-duedate");
-const saveButton = modalBoxContent.querySelector(".save-btn");
-const deleteButton = modalBoxContent.querySelector(".delete-btn");
+const title = document.querySelector(".input-title");
+const titleLabel = document.querySelector(".label-title");
+const description = document.querySelector(".input-description");
+const duedate = document.querySelector(".input-duedate");
+const saveButton = document.querySelector(".save-btn");
+const deleteButton = document.querySelector(".delete-btn");
 
 const events = JSON.parse(localStorage.getItem("events")) || [];
 let selectedDay;
 let selectedDayElement;
-
-// when an event is added the calendar should be rendered again
-function saveEvent(e) {
-    const eventData = {
-        id: selectedDay,
-        title: title.value,
-        description: description.value,
-        duedate: duedate.value
-    }
-
-    events.push(eventData);
-    localStorage.setItem("events", JSON.stringify(events));
-    console.log(events);
-}
 
 function displayEventData(day) {
     events.forEach(event => {
         if (event.id === day) {
             title.value = event.title;
             description.value = event.description;
-            duedate.value = "01/01/0000";
+            duedate.value = dayjs(event.duedate).format("YYYY-MM-DD");
 
-            // this could go in a function x2
             titleLabel.classList.add("has-content");
             saveButton.textContent = "Update";
             saveButton.style.margin = "2rem 0 0 5rem";
             deleteButton.style.display = "inline-block";
         } 
+    });
+}
+
+/** 
+ * Saves or updates the event based on the filteredEvents length. 
+ * If the filteredEvents length is less than the events length then
+ * that's because there's a repeated event (said event is filtered at
+ * the beginning of the function, that's why its less) so what's saved
+ * is the new, no-repeat version of the events array.
+*/
+function saveEvent(e) {
+    const filteredEvents = events.filter(e => e.id !== selectedDay);
+    const eventData = {
+        id: selectedDay,
+        title: title.value,
+        description: description.value,
+        duedate: duedate.value
+    };
+
+    if (events.length > filteredEvents.length) {
+        filteredEvents.push(eventData);
+        console.log(filteredEvents);
+        localStorage.setItem("events", JSON.stringify(filteredEvents));
+    } else {
+        events.push(eventData);
+        localStorage.setItem("events", JSON.stringify(events));
+        console.log(events);
+    }
+}
+
+function deleteEvent(e) {
+    [...calendarDays.children].forEach(day => {
+        let currentDay = day.dataset.id;
+        if (currentDay === selectedDay) {
+            selectedDayElement.lastChild.classList.remove("event-active");
+            const remainingEvents = events.filter(e => e.id !== currentDay);
+            localStorage.setItem("events", JSON.stringify(remainingEvents));
+
+            modalBox.style.display = "none";
+        }
     });
 }
 
@@ -58,14 +84,13 @@ function handleDayClick() {
     window.addEventListener("click", e => {
         if (e.target !== modalBox) return;
 
-        // this could go in a function
         title.value = "";
         description.value = "";
+        duedate.value = "";
         titleLabel.classList.remove("has-content");
         deleteButton.style.display = "none";
         saveButton.textContent = "Save";
         saveButton.style.margin = "2rem 0 0 8.5rem";
-    
         modalBox.style.display = "none";
     });
     title.addEventListener("keyup", e => {
@@ -77,18 +102,7 @@ function handleDayClick() {
     });
     saveButton.addEventListener("click", saveEvent);
 
-    // At the moment only the icon is being deleted
-    deleteButton.addEventListener("click", () => {
-        [...calendarDays.children].forEach(day => {
-            let current = day.dataset.id;
-            if (current === selectedDay) {
-                selectedDayElement.lastChild.classList.remove("event-active");
-                const remainingEvents = events.filter(e => e.id !== current);
-                localStorage.setItem("events", JSON.stringify(remainingEvents));
-                modalBox.style.display = "none";
-            }
-        });
-    });
+    deleteButton.addEventListener("click", deleteEvent);
     closeModalBox.addEventListener("click", () => modalBox.style.display="none");;
 }
 
