2cdf0783dd886c91d51a2b43c5eacf5197c28b34
Zondazx <mzxgd16@gmail.com>
2020-10-27 09:26:10 -0400

    feat: optional duedate, bug fixes

--- src/scripts/CalendarEvents.js
+++ src/scripts/CalendarEvents.js
@@ -12,76 +12,18 @@ const description = document.querySelector(".input-description");
 const duedate = document.querySelector(".input-duedate");
 const saveButton = document.querySelector(".save-btn");
 const deleteButton = document.querySelector(".delete-btn");
+const errorMessage = document.querySelector(".error");
 
+const activeDuedate = document.getElementById("active-duedate");
+
+/**
+ * TODO: 
+ * - Stop using global reference ?
+ * - Validating the title (if its set) only with HTML ?
+ */
 let events = JSON.parse(localStorage.getItem("events")) || [];
 let selectedDay;
 let selectedDayElement;
-
-function displayEventData(day) {
-    events.forEach(event => {
-        if (event.id === day) {
-            title.value = event.title;
-            description.value = event.description;
-            duedate.value = dayjs(event.duedate).format("YYYY-MM-DD");
-
-            titleLabel.classList.add("has-content");
-            saveButton.textContent = "Update";
-            saveButton.style.margin = "2rem 0 0 5rem";
-            deleteButton.style.display = "inline-block";
-        } 
-    });
-}
-
-function cleanEventData() {
-    title.value = "";
-    description.value = "";
-    duedate.value = "";
-    
-    titleLabel.classList.remove("has-content");
-    saveButton.textContent = "Save";
-    saveButton.style.margin = "2rem 0 0 8.5rem";
-    deleteButton.style.display = "none";
-    modalBox.style.display = "none";
-}
-
-/** 
- * Saves or updates the event based on the filteredEvents length. 
- * If the filteredEvents length is less than the events length then
- * that's because there's a repeated event (said event is filtered at
- * the beginning of the function, that's why its less) so what's saved
- * is the new, no-repeat version of the events array.
-*/
-function saveEvent(e) {
-    const filteredEvents = events.filter(e => e.id !== selectedDay);
-    const eventFlag = selectedDayElement.lastChild;
-    const eventData = {
-        id: selectedDay,
-        title: title.value,
-        description: description.value,
-        duedate: duedate.value
-    };
-
-    if (events.length > filteredEvents.length) {
-        filteredEvents.push(eventData);
-        events = filteredEvents;
-        console.log(filteredEvents);
-    } else {
-        events.push(eventData);
-        console.log(events);
-    }
-    
-    eventFlag.classList.add("event-active");
-    localStorage.setItem("events", JSON.stringify(events));
-}
-
-function deleteEvent() {
-    const eventFlag = selectedDayElement.lastChild;
-    eventFlag.classList.remove("event-active");
-    events = events.filter(e => e.id !== selectedDay);
-
-    localStorage.setItem("events", JSON.stringify(events));
-    modalBox.style.display = "none";
-}
 
 function handleDayClick() {
     calendarDays.addEventListener("click", e => {
@@ -94,19 +36,119 @@ function handleDayClick() {
     });
     window.addEventListener("click", e => {
         if (e.target !== modalBox) return;
-        
+
         cleanEventData();
     });
     title.addEventListener("keyup", e => {
-        if (e.target.value === "" || !e.target.value) {
+        if (isEmpty(e.target.value)) {
             titleLabel.classList.remove("has-content");
         } else {
             titleLabel.classList.add("has-content");
         }
     });
+    activeDuedate.addEventListener("click", () => {
+        if (activeDuedate.checked) {
+            duedate.disabled = true;
+        } else {
+            duedate.disabled = false;
+        }
+    });
     saveButton.addEventListener("click", saveEvent);
     deleteButton.addEventListener("click", deleteEvent);
-    closeModalBox.addEventListener("click", () => modalBox.style.display="none");;
+    closeModalBox.addEventListener("click", () => modalBox.style.display = "none");;
 }
 
-export { handleDayClick };
+function displayEventData(day) {
+    events.forEach(event => {
+        if (event.id === day) {
+            title.value = event.title;
+            description.value = event.description;
+            duedate.value = dayjs(event.duedate).format("YYYY-MM-DD");
+
+            titleLabel.classList.add("has-content");
+            saveButton.textContent = "Update";
+        
+            saveButton.classList.remove("align-save-btn");
+            saveButton.classList.add("align-btns");
+            deleteButton.style.display = "inline-block";
+        }
+    });
+}
+
+function cleanEventData() {
+    title.value = "";
+    description.value = "";
+    duedate.value = "";
+
+    titleLabel.classList.remove("has-content");
+    saveButton.textContent = "Save";
+
+    saveButton.classList.remove("align-btns");
+    saveButton.classList.add("align-save-btn");
+    deleteButton.style.display = "none";
+    modalBox.style.display = "none";
+}
+
+function isEmpty(input) {
+    return input === "" || !input;
+}
+
+/** 
+ * Saves or updates the event based on the filteredEvents length. 
+ * If the filteredEvents length is less than the events length then
+ * that's because there's a repeated event (said event is filtered at
+ * the beginning of the function, that's why its less) so what's saved
+ * is the new, no-repeat version of the events array.
+ */
+function saveEvent(e) {
+    const filteredEvents = events.filter(e => e.id !== selectedDay);
+    const eventFlag = selectedDayElement.lastChild;
+    const eventData = {
+        id: selectedDay,
+        title: title.value,
+        description: description.value,
+        duedate: duedate.value
+    };
+
+    if (isEmpty(eventData.title)) {
+        errorMessage.classList.add("display-error");
+        saveButton.classList.add("disabled-button");
+        saveButton.disabled = true;
+
+        setTimeout(() => {
+            saveButton.disabled = false;
+            saveButton.classList.remove("disabled-button");
+            errorMessage.classList.remove("display-error");
+        }, 5000);
+
+    } else {
+
+        if (events.length > filteredEvents.length) {
+            filteredEvents.push(eventData);
+            events = filteredEvents;
+            console.log(filteredEvents);
+        } else {
+            events.push(eventData);
+            console.log(events);
+        }
+
+        eventFlag.classList.add("event-active");
+        localStorage.setItem("events", JSON.stringify(events));
+        modalBox.style.display = "none";
+    }
+}
+
+function deleteEvent() {
+    const eventFlag = selectedDayElement.lastChild;
+    eventFlag.classList.remove("event-active");
+    events = events.filter(e => e.id !== selectedDay);
+
+    localStorage.setItem("events", JSON.stringify(events));
+
+    modalBox.style.display = "none";
+    cleanEventData();
+}
+
+export {
+    handleDayClick
+};
