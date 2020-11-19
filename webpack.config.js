e4b63fda8c70f748d60356e8791996eda4fdcfd6
Zondazx <mzxgd16@gmail.com>
2020-11-19 18:54:11 -0400

    fix: calendar.js name change fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,47 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/scripts/Calendar.js",
-    output: {
-        path: path.resolve(__dirname, "./build"),
-        filename: "bundle.js" 
-    },
-    devServer: {
-        port: 5000
-    },
-    module :{
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader },
-                    { loader: "css-loader" }
-                ]
-            }, 
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext]',
-                            context: path.resolve(__dirname, "images/"),
-                            outputPath: 'images/',
-                            publicPath: 'images/',
-                            useRelativePaths: true
-                        }
-                    }
-                ] 
-            }
+  mode: "development",
+  entry: "./src/js/calendar.js",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: 5000
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: [{
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
-        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+            context: path.resolve(__dirname, "images/"),
+            outputPath: 'images/',
+            publicPath: 'images/',
+            useRelativePaths: true
+          }
+        }]
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
 }
